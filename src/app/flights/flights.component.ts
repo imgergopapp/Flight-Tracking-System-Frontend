1a9aa67d52efc601385035b93c8ad45599f8d5aa
Gergő Papp <imgergopapp@gmail.com>
2019-10-14 08:12:20 +0200

    Create CRUD requests

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-flights',
@@ -9,7 +9,15 @@ import { HttpClient } from '@angular/common/http';
 export class FlightsComponent implements OnInit {
 
   flights = null;
-  ApiUrl = "http://localhost:8080";
+  company = "";
+  estimatedDeparture = "";
+  departure = "";
+  arrival = "";
+  today = Date.now();
+  apiUrl = "http://localhost:8080";
+  departureFrom;
+  departureTo;
+
 
   constructor(
     private http: HttpClient
@@ -21,13 +29,51 @@ export class FlightsComponent implements OnInit {
 
 
   getFlights() {
-    let url = this.ApiUrl + '/flights';
+    let url = this.apiUrl + '/flights';
     this.http.get(url).subscribe(res => this.setFlights(res),
       error => console.log(error)); { };
   }
 
   setFlights(flights) {
     this.flights = flights;
+
+  }
+
+  filterByDeparture() {
+    if(this.departureTo == undefined || this.departureFrom == undefined){
+      return;
+    }
+    let url = this.apiUrl + '/filter/flights';
+    let params = new HttpParams().set("departureFrom", this.departureFrom).set("departureTo", this.departureTo);
+    this.http.get(url, { params: params }).subscribe(res => this.getFlights(),
+      error => console.log(error)); { };
+  }
+
+  updateFlight(id, company) {
+    let url = this.apiUrl + '/flights';
+    this.http.put<any>(url, {
+      id: id,
+      company: company
+    }).subscribe(res => this.getFlights(),
+      error => console.log(error)); { };
+  }
+
+  deleteFlight(id) {
+    let url = this.apiUrl + '/flights/' + id;
+    let params = new HttpParams().set("id", id);
+    this.http.delete(url, { params: params }).subscribe(res => this.getFlights(),
+      error => console.log(error)); { };
+  }
+
+  createFlight() {
+    let url = this.apiUrl + '/flights';
+    this.http.post<any>(url, {
+      company: this.company,
+      estimatedDeparture: this.estimatedDeparture,
+      departure: this.departure,
+      arrival: this.arrival
+    }).subscribe(res => this.getFlights(),
+      error => console.log(error)); { };
   }
 
 }
